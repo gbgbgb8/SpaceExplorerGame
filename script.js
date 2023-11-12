475c2ce62659ae13f47091dd43423de67251d56f
Nota Name <email@thisisnotanemail.com>
2023-11-12 10:03:48 -0800

    init2

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+let canvas;
+let player;
+let powerEmojis = [];
+let score = 0;
+let startTime;
+let gameSpeed = 1;
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    player = new Player();
+    startTime = millis();
+    for (let i = 0; i < 10; i++) {
+        powerEmojis.push(new PowerEmoji());
+    }
+    textAlign(CENTER, CENTER);
+    textSize(28);
+}
+
+function draw() {
+    drawStarfield();
+    player.display();
+    player.move();
+
+    for (let emoji of powerEmojis) {
+        emoji.display();
+        emoji.move();
+        if (player.collects(emoji)) {
+            score++;
+            gameSpeed += 0.1;
+            powerEmojis.splice(powerEmojis.indexOf(emoji), 1);
+            powerEmojis.push(new PowerEmoji());
+        }
+    }
+
+    updateScoreAndTimer();
+    checkGameOver();
+}
+
+function updateScoreAndTimer() {
+    document.getElementById('score').innerText = `Score: ${score}`;
+    let elapsedTime = int((millis() - startTime) / 1000);
+    document.getElementById('timer').innerText = `Time: ${nf(elapsedTime, 2, 0)}`;
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
 class Player {
     constructor() {
         this.size = 30;
@@ -7,7 +55,7 @@ class Player {
 
     display() {
         fill(255);
-        text('🚀', this.x, this.y); // Displaying rocket emoji as player
+        text('🚀', this.x, this.y);
     }
 
     move() {
@@ -16,7 +64,6 @@ class Player {
         if (keyIsDown(UP_ARROW)) this.y -= 5 * gameSpeed;
         if (keyIsDown(DOWN_ARROW)) this.y += 5 * gameSpeed;
 
-        // Keep the player within the canvas boundaries
         this.x = constrain(this.x, 0, width - this.size);
         this.y = constrain(this.y, 0, height - this.size);
     }
@@ -36,28 +83,28 @@ class PowerEmoji {
 
     display() {
         fill(255);
-        text('⚡', this.x, this.y); // Displaying power emoji
+        text('⚡', this.x, this.y);
     }
 
     move() {
-        // Logic for moving the power emoji goes here
-        // For now, they will be stationary
+        // Power Emojis are stationary for now
     }
 }
 
-function touchMoved() {
-    // Mobile touch controls
-    player.x = touchX;
-    player.y = touchY;
-    return false;
+function drawStarfield() {
+    for (let i = 0; i < 100; i++) {
+        let x = random(width);
+        let y = random(height);
+        stroke(255);
+        point(x, y);
+    }
 }
 
-// Add event listener for touch controls
-const hammer = new Hammer(canvas.elt);
-hammer.on('pan', function(event) {
-    player.x = event.center.x;
-    player.y = event.center.y;
-});
+function checkGameOver() {
+    if (score >= 10) {
+        resetGame();
+    }
+}
 
 function resetGame() {
     score = 0;
@@ -69,31 +116,15 @@ function resetGame() {
     startTime = millis();
 }
 
-function checkGameOver() {
-    if (score >= 10) {
-        resetGame();
-    }
-}
+// Add event listener for touch controls
+const hammer = new Hammer(canvas.elt);
+hammer.on('pan', function(event) {
+    player.x = event.center.x;
+    player.y = event.center.y;
+});
 
-function drawStarfield() {
-    // Starfield background logic goes here
-    // Placeholder: fill the background with white dots
-    for (let i = 0; i < 100; i++) {
-        let x = random(width);
-        let y = random(height);
-        stroke(255);
-        point(x, y);
-    }
-}
-
-function setup() {
-    // ... existing setup code ...
-    textAlign(CENTER, CENTER);
-    textSize(28);
-}
-
-function draw() {
-    drawStarfield();
-    // ... existing draw code ...
-    checkGameOver();
+function touchMoved() {
+    player.x = touchX;
+    player.y = touchY;
+    return false;
 }
