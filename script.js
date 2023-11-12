b831dd4a85e8c5c8af1a2e01ab0413ea8206a3e1
Nota Name <email@thisisnotanemail.com>
2023-11-12 10:13:00 -0800

    1013

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let canvas;
 let player;
 let powerEmojis = [];
+let stars = [];
 let score = 0;
 let startTime;
 let gameSpeed = 1;
@@ -12,8 +13,11 @@ function setup() {
     for (let i = 0; i < 10; i++) {
         powerEmojis.push(new PowerEmoji());
     }
+    for (let i = 0; i < 200; i++) {
+        stars.push(new Star());
+    }
     textAlign(CENTER, CENTER);
-    textSize(28);
+    textSize(50);
 }
 
 function draw() {
@@ -48,7 +52,7 @@ function windowResized() {
 
 class Player {
     constructor() {
-        this.size = 30;
+        this.size = 60;
         this.x = width / 2;
         this.y = height / 2;
     }
@@ -70,13 +74,13 @@ class Player {
 
     collects(emoji) {
         let d = dist(this.x, this.y, emoji.x, emoji.y);
-        return d < this.size;
+        return d < this.size / 2 + emoji.size / 2;
     }
 }
 
 class PowerEmoji {
     constructor() {
-        this.size = 20;
+        this.size = 40;
         this.x = random(this.size, width - this.size);
         this.y = random(this.size, height - this.size);
     }
@@ -91,12 +95,41 @@ class PowerEmoji {
     }
 }
 
+class Star {
+    constructor() {
+        this.x = random(-width, width);
+        this.y = random(-height, height);
+        this.z = random(width);
+    }
+
+    update() {
+        this.z -= 10;
+        if (this.z < 1) {
+            this.z = width;
+            this.x = random(-width, width);
+            this.y = random(-height, height);
+        }
+    }
+
+    show() {
+        fill(255);
+        noStroke();
+
+        let sx = map(this.x / this.z, 0, 1, 0, width);
+        let sy = map(this.y / this.z, 0, 1, 0, height);
+
+        let r = map(this.z, 0, width, 16, 0);
+        ellipse(sx, sy, r, r);
+    }
+}
+
 function drawStarfield() {
-    for (let i = 0; i < 100; i++) {
-        let x = random(width);
-        let y = random(height);
-        stroke(255);
-        point(x, y);
+    background(0); // Set the background to black
+    translate(width / 2, height / 2); // Move the origin to the center
+
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].update();
+        stars[i].show();
     }
 }
 
@@ -116,7 +149,6 @@ function resetGame() {
     startTime = millis();
 }
 
-// Add event listener for touch controls
 const hammer = new Hammer(canvas.elt);
 hammer.on('pan', function(event) {
     player.x = event.center.x;
